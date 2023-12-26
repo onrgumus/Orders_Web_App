f68a513cdc65302478cf1cb32aa130422bae9241
Onur Gumus <80348608+onrgumus@users.noreply.github.com>
2023-12-26 17:51:06 +0300

    Siparis.py

--- pages/Siparis.py
+++ pages/Siparis.py
@@ -2,7 +2,6 @@ import streamlit as st
 import sqlite3
 from pathlib import Path
 
-
 # Streamlit sayfa yapılandırması
 st.set_page_config(
     page_title="Create Orders",
@@ -14,6 +13,7 @@ st.set_page_config(
 # --- DOSYA YOLLARI ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
+
 conn = sqlite3.connect("pizzadb.sqlite3")
 c = conn.cursor()
 
@@ -23,10 +23,7 @@ conn.commit()
 c.execute("SELECT isim FROM pizzalar")
 isimler = c.fetchall()
 
-isimlerlist = []
-
-for i in isimler:
-    isimlerlist.append(i[0])
+isimlerlist = [i[0] for i in isimler]
 
 st.header('Orders')
 
@@ -39,30 +36,33 @@ with st.form("Sipariş", clear_on_submit=True):
     siparisver = st.form_submit_button("Place Order")
     
     if siparisver:
-        if boy == "Small":
-            c.execute("SELECT smfiyat FROM pizzalar WHERE isim = ?", (pizza,))
-        elif boy == "Medium":
-            c.execute("SELECT mdfiyat FROM pizzalar WHERE isim = ?", (pizza,))
-        elif boy == "Large":
-            c.execute("SELECT lgfiyat FROM pizzalar WHERE isim = ?", (pizza,))
-        
-        fiyat = c.fetchone()[0]  # Fiyatı alır
-        
-        icecek_fiyatları = {
-            "Ayran": 15,
-            "Cola": 40,
-            "Soda": 30,
-            "Su": 5,
-            "Ice Tea": 30
-        }
-        
-        icecek_fiyati = icecek_fiyatları[icecek]  # Seçilen içeceğin fiyatını alır
-        
-        toplam_fiyat = fiyat + icecek_fiyati  # Toplam fiyatı hesaplar
-        
-        c.execute("INSERT INTO siparisler VALUES(?,?,?,?,?,?)", (isim_soyisim, Adress, pizza, boy, icecek, toplam_fiyat))
-        conn.commit()
-        
-        st.success(f"Sipariş Başarılı Bir Şekilde Gerçekleştirildi. Toplam Ücret: {toplam_fiyat} ₺")
+        if isim_soyisim and Adress and pizza and boy and icecek:  # Tüm alanlar dolu mu?
+            if boy == "Small":
+                c.execute("SELECT smfiyat FROM pizzalar WHERE isim = ?", (pizza,))
+            elif boy == "Medium":
+                c.execute("SELECT mdfiyat FROM pizzalar WHERE isim = ?", (pizza,))
+            elif boy == "Large":
+                c.execute("SELECT lgfiyat FROM pizzalar WHERE isim = ?", (pizza,))
+            
+            fiyat = c.fetchone()[0]  # Fiyatı alır
+            
+            icecek_fiyatları = {
+                "Ayran": 15,
+                "Cola": 40,
+                "Soda": 30,
+                "Su": 5,
+                "Ice Tea": 30
+            }
+            
+            icecek_fiyati = icecek_fiyatları[icecek]  # Seçilen içeceğin fiyatını alır
+            
+            toplam_fiyat = fiyat + icecek_fiyati  # Toplam fiyatı hesaplar
+            
+            c.execute("INSERT INTO siparisler VALUES(?,?,?,?,?,?)", (isim_soyisim, Adress, pizza, boy, icecek, toplam_fiyat))
+            conn.commit()
+            
+            st.success(f"Order Successfully Placed. Total Amount: {toplam_fiyat} ₺")
+        else:
+            st.warning("Please fill in all fields.")
 
 conn.close()
